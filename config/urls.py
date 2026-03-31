f0b46ce1cc4d5192c844e4cf9a20ac82049a35e6
Erick Martinez <erickmartinez0131@gmail.com>
2026-03-30 19:54:00 -0500

    django startapp | env setup | req.txt | home_view | templates

---
+++ config/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from core.views import home_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home_view),
+]
