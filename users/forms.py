7ab305280d82a91eaa21f544ee9cdbab1044e4c9
Erick Martinez <erickmartinez0131@gmail.com>
2026-04-28 13:17:24 -0500

    parsing for baylor emails only

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from .models import User
 
@@ -18,6 +19,14 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'graduation_year', 'password1', 'password2')
+
+    def clean_email(self) -> str:
+        email: str = self.cleaned_data['email'].lower()
+        if not email.endswith('@baylor.edu'):
+            raise ValidationError('You must use a Baylor University email address (@baylor.edu).')
+        if User.objects.filter(email__iexact=email).exists():
+            raise ValidationError('An account with this email address already exists.')
+        return email
 
     def save(self, commit: bool = True) -> User:
         user = super().save(commit=False)
