fd8e0980cce8c0255060fedb88b2e1482225b231
Erick Martinez <erickmartinez0131@gmail.com>
2026-05-07 16:53:02 -0500

    aggregate mutliple items to pickup at a time

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 from core.views import (
     admin_accept_quote_view,
     admin_kanban_approve_view,
+    admin_kanban_bulk_pickup_view,
     admin_kanban_assign_admin_view,
     admin_kanban_deny_view,
     admin_kanban_pickup_label_view,
@@ -38,6 +39,7 @@ urlpatterns = [
     path('admin-quotes/accept/<int:quote_id>/', admin_accept_quote_view, name='admin_accept_quote'),
     path('admin-dashboard/', admin_kanban_view, name='admin_kanban'),
     path('admin-dashboard/approve/<int:quote_id>/', admin_kanban_approve_view, name='admin_kanban_approve'),
+    path('admin-dashboard/bulk-pickup/', admin_kanban_bulk_pickup_view, name='admin_kanban_bulk_pickup'),
     path('admin-dashboard/assign-admin/<int:quote_id>/', admin_kanban_assign_admin_view, name='admin_kanban_assign_admin'),
     path('admin-dashboard/pickup-label/<int:quote_id>/', admin_kanban_pickup_label_view, name='admin_kanban_pickup_label'),
     path('admin-dashboard/pickup/<int:quote_id>/', admin_kanban_pickup_view, name='admin_kanban_pickup'),
